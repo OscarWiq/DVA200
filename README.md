8d422a737e307dfd83b4dda2a1811b6d9b0df22f
OscarWiq <owiqvist@gmail.com>
2020-04-20 10:56:42 +0200

    added info to README

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # DVA200
+
+## Installation
+
+pip3 install virtualenv
+
+### Mac/Linux
+```
+$ virtualenv venv
+$ source venv/bin/activate
+(venv) $ pip3 install -r deps.txt
+```
+
+### Windows
+```
+virtualenv venv
+venv\Scripts\activate
+(venv) $ pip3 install -r deps.txt
+```
+
+## Körning
+```
+(venv) $ python3 server.py
+* Running on http://127.0.0.1:5000/
+* Restarting with reloader
+```
---
+++ server.py
@@ -0,0 +1,58 @@
+
+from flask import Flask, jsonify, request, abort, make_response, url_for
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import json
+
+app = Flask(__name__, static_url_path = "")
+auth = HTTPBasicAuth()
+
+param_id = "id"
+param_header = "header"
+param_body = "body"
+param_time = "time"
+param_user = "username"
+param_result = "result"
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password) 
+    return False
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({param_result: "403, Unauthorized access"}), 403)
+
+@app.errorhandler(400)
+def not_found():
+    return make_response(jsonify({param_result: "400, Bad request"}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({param_result: "404, Not found"}), 404)
+
+with open ("usrs.json", "r") as u:
+    usrs = json.load(u)
+users = usrs
+print(str(users))
+
+@app.route("/login", methods=["GET"])
+@app.route("/login/", methods=["GET"])
+@auth.login_required
+def index():
+    return jsonify({param_result: [True, "Hello, %s!" % auth.username()]})
+
+if __name__ == "__main__":
+    app.run(debug=True, ssl_context='adhoc')
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
